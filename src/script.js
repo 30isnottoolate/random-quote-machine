e2bfbd79fc920205b241ef617a0c2c3ce1458c89
30isnottoolate <akos.varga.92@gmail.com>
2022-07-08 17:59:33 +0200

    Change the main component into a stateful one
        Converted RandomQuoteMachine component into a stateful component.
        Connected component states to the innerText of <p> elements.

--- src/script.js
+++ src/script.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './app_styles.css';
 
 class RandomQuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      numOfQuotes: 0,
+      quote: "Loading...",
+      author: ""
+    };
+  }
   render() {
     return (
       <div id="quote-machine">
-        <p id="text">Quote</p>
-        <p id="author">Author</p>
+        <p id="text">{this.state.quote}</p>
+        <p id="author">{this.state.author}</p>
         <button id="new-quote">New Quote</button>
       </div>
     );
