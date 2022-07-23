59004131eb761e4489bb21e81ff450d0613a755b
30isnottoolate <107811595+30isnottoolate@users.noreply.github.com>
2022-07-23 16:43:56 +0200

    Render RandomQuoteMachine from index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './script.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RandomQuoteMachine from './RandomQuoteMachine';
+
+const container = ReactDOM.createRoot(document.getElementById('app-container'));
+container.render(
+  <React.StrictMode>
+    <RandomQuoteMachine />
+  </React.StrictMode>
+);
