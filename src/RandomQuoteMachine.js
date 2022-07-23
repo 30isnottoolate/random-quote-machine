14f61b14d76bb62187fb4fbdf34d7c1e35e28664
30isnottoolate <107811595+30isnottoolate@users.noreply.github.com>
2022-07-23 16:39:35 +0200

    Rename files

--- src/RandomQuoteMachine.js
+++ src/RandomQuoteMachine.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app_styles.css';
+import './RandomQuoteMachine.css';
 
 class RandomQuoteMachine extends React.Component {
   constructor(props) {
